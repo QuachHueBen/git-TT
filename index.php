449e8e64654493bd2c8c256aa8bd6394770cf125
Quách Huệ Bền <95475062+QuachHueBen@users.noreply.github.com>
2021-12-04 12:08:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
+    <?php wp_head();?>
+</head>
+<body <?php body_class();?> >
+    <div class="wrapper">
+        <div class="header-wrapper relative z-20">
+            <header class="relative z-10">
+                <div class="absolute left-0 2xl:left-[201px] top-[1px]">
+                    <img src="./wp-content/uploads/2021/11/Little-Little-Logo-ngang-1-1.png" alt="logo" >
+                </div>
+                    <!-- <?php tittle_menu('primary-menu'); ?> -->
+                    <ul class="absolute  z-10 top-[29px] right-0 text-right sm:block md:block lg:flex items-center justify-center 2xl:flex">
+                        <li><a href="#">Trang chủ</a></li>
+                        <li><a href="#">Sự kiện</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                        <li><a href="#">0123456789</a></li>
+                    </ul>
+            </header>
+        </div>
+        <div class="absolute w-full h-full bg-no-repeat top-10 left-9 "
+           style="background-image: url('/TTAP-TN/wp-content/uploads/2021/11/bg.png')">
+        </div>
+        <!-- <div class=" object-cover absolute top-28 left-1 lg:left-9 z-0">
+            <img src="./wp-content/uploads/2021/11/bg.png" alt="">
+        </div> -->
+        <div class="">
+            <img src="./wp-content/uploads/2021/12/image-2-1.png" alt="" 
+            class="absolute z-10 left-7 top-32  w-20 h-20 lg:left-[200px] lg:top-[148.59px] lg:w-48 lg:h-36 ">
+            <p class="absolute w-32 h-11 text-xl left-32 top-32  
+            lg:w-96 lg:h-24 lg:left-[397px] lg:top-[132px] lg:text-7xl font-black text-white ">ĐẦM SEN</p>
+            <p class="absolute w-12 h-11 text-xl left-32 top-40 
+            lg:left-[402px] lg:top-[208px] lg:text-7xl font-black text-white lg:w-48 lg:h-24">PARK</p>
+        </div>
+        <div class="absolute w-20 h-20 top-52 lg:left-[18px] lg:top-[321px] lg:w-full lg:h-full ">
+            <img src="./wp-content/uploads/2021/11/18451-Converted-06-1.png" alt="logo">
+        </div>
+        <div class="absolute top-64 z-10 w-32 h-32 lg:top-[528px] lg:w-full lg:h-full ">
+            <img src="./wp-content/uploads/2021/11/Lisa_Arnold_Lay_Do_F2-3.png" alt="">
+        </div>
+        <div class="absolute w-20 h-20 top-32 left-56  xl:left-[807px] 2xl:top-[174.02px] 2xl:w-full 2xl:h-full ">
+            <img src="./wp-content/uploads/2021/11/18451-Converted-03-1.png" alt="">
+        </div>
+        <div class="absolute xl:left-[1030px] xl:top-[93.93px]">
+            <img src="./wp-content/uploads/2021/11/18451-Converted-02-1.png" alt="">
+        </div>
+        <div class="absolute w-80 h-80 top-24 right-0 xl:left-[600px] xl:w-full xl:h-full 2xl:left-[1052px] 2xl:top-[23px] 2xl:w-full 2xl:h-full">
+            <img src="./wp-content/uploads/2021/11/render-fix-hair-1.png" alt="">
+        </div>
+        <div class="absolute xl:left-[630px] xl:top-[910.65px] xl:bottom-[6.76px]">
+            <img src="./wp-content/uploads/2021/11/18451-Converted-04-1.png" alt="logo">
+        </div>
+        <div class="absolute lg:bottom-[36.11px] xl:left-[1693px] xl:top-[803.04] xl:bottom-[36.11px]">
+            <img src="./wp-content/uploads/2021/11/18451-Converted-03-1.png" alt="">
+        </div>
+        <div class=" absolute z-10 xl:left-[1309px] xl:top-[333.48px] xl:bottom-[659.26px]">
+            <img src="./wp-content/uploads/2021/11/Group-1.png" alt="">
+        </div>
+        <div class="">
+            <img src="./wp-content/uploads/2021/11/Group.png" alt="" 
+            class="absolute  2xl:w-[950px] 2xl:h-[600px] 2xl:left-[201px] 2xl:top-[366.02px] 2xl:bottom-[115px]">
+            <img src="./wp-content/uploads/2021/11/Vector.png" alt="" 
+            class="absolute h-auto 2xl:w-[141.04px] 2xl:h-[583.79px] 2xl:left-[1108px] 2xl:top-[365.01px] 2xl:bottom-[131.19px]">
+            <img src="./wp-content/uploads/2021/11/Group-1-1.png" alt="" 
+            class="absolute h-auto 2xl:w-[539px] 2xl:h-[599.75px] 2xl:left-[1181px] 2xl:top-[365.25px] 2xl:bottom-[115px]">
+        </div>
+
+    </div>
+<?php wp_footer(); ?>
+</body>
+</html>
